afb4fbe1e6feded1635c06c18a44de16cb873909
Saengsri <jeffsaengsri@gmail.com>
2016-12-28 11:00:33 -0800

    Delete jsaengsriPersonal.js

--- js/jsaengsriPersonal.js
+++
@@ -1,9 +0,0 @@
-function main() {
-	$(".description").hide();
-	$(".learn-more-button").on("click", function(){
-		$(this).next().slideToggle(400);
-		$(this).toggleClass("active").next();
-	});
-}
-  $(document).ready(main);
-	
